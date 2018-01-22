bf06400cf90481a8009c83deb781a86235ad5a6d
FOKRUL Amin (AXA-H) <Amin Fokrul@axa-uk-test.intraxa>
2018-01-22 10:31:35 +0000

    initial commit

---
+++ SOLIDPrinciples/SingleResponsibility/OrderFixed.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLIDPrinciples.SingleResponsibility
+{
+    public class OrderFixed // this could be an abstract class to be overriden by other order type class
+    {
+        public int OrderId { get; set; }
+
+        public Basket Basket { get; set; }
+
+        public OrderFixed(Basket basket)
+        {
+            Basket = basket;
+        }
+
+        public void ProcessOrder(Basket basket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Basket
+    {
+        public IEnumerable<int> Products { get; set; }
+        public int BasketTotal { get; set; }
+    }
+}
---
+++ SOLIDPrinciples/SingleResponsibility/OrderProblem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SOLIDPrinciples.SingleResponsibility
+{
+    public class OrderProblem
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public IEnumerable<int> Products { get; set; }
+        public int CreditCardId { get; set; }
+
+        public void ChargeCard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProcessOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckStock()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
